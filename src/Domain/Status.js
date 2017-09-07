746010dd3796b0656280f08b5141ca11eafb2ee7
Sasha Sushko <sushko@outlook.com>
2017-09-07 19:19:37 +0500

    Fixed #16 and #29

--- src/Domain/Status.js
+++ src/Domain/Status.js
@@ -3,21 +3,27 @@ export const Statuses = {
     NODATA: 'NODATA',
     WARN: 'WARN',
     ERROR: 'ERROR',
+    EXCEPTION: 'EXCEPTION',
+    DEL: 'DEL',
     OK: 'OK',
 };
 
 export const StatusesCaptions = {
-    OK: 'OK',
     NODATA: 'NODATA',
     WARN: 'WARN',
     ERROR: 'ERROR',
+    EXCEPTION: 'EXCEPTION',
+    DEL: 'DEL',
+    OK: 'OK',
 };
 
 export const StatusesColors = {
-    OK: '#00bfa5',
     NODATA: '#9e9e9e',
     WARN: '#ffc107',
     ERROR: '#ff5722',
+    EXCEPTION: '#ff5722',
+    DEL: '#000',
+    OK: '#00bfa5',
 };
 
 export type Status = $Keys<typeof Statuses>;
