1b555e05daf3698fc0f6413be9f357d4dcc7482a
Sasha Sushko <sushko@outlook.com>
2017-09-13 15:42:48 +0500

    Fixed #42

--- src/Containers/PatternListContainer.js
+++ src/Containers/PatternListContainer.js
@@ -4,6 +4,7 @@ import type { ContextRouter } from 'react-router-dom';
 import type { IMoiraApi } from '../Api/MoiraAPI';
 import type { Pattern } from '../Domain/Pattern';
 import { withMoiraApi } from '../Api/MoiraApiInjection';
+import PatternList from '../Components/PatternList/PatternList';
 import Layout from '../Components/Layout/Layout';
 
 type Props = ContextRouter & { moiraApi: IMoiraApi };
@@ -22,11 +23,16 @@ class PatternListContainer extends React.Component {
     };
 
     componentDidMount() {
-        this.getData();
+        this.getData(this.props);
     }
 
-    async getData(): Promise<void> {
-        const { moiraApi } = this.props;
+    componentWillReceiveProps(nextProps: Props) {
+        this.setState({ loading: true });
+        this.getData(nextProps);
+    }
+
+    async getData(props: Props): Promise<void> {
+        const { moiraApi } = props;
         try {
             const patterns = await moiraApi.getPatternList();
             this.setState({ loading: false, ...patterns });
@@ -36,12 +42,25 @@ class PatternListContainer extends React.Component {
         }
     }
 
+    async removePattern(pattern: string): Promise<void> {
+        this.setState({ loading: true });
+        await this.props.moiraApi.delPattern(encodeURI(pattern));
+        this.getData(this.props);
+    }
+
     render(): React.Element<*> {
         const { loading, error, list } = this.state;
         return (
             <Layout loading={loading} error={error}>
                 <Layout.Content>
-                    <pre>{JSON.stringify(list, null, 2)}</pre>
+                    {list && (
+                        <PatternList
+                            items={list}
+                            onRemove={pattern => {
+                                this.removePattern(pattern);
+                            }}
+                        />
+                    )}
                 </Layout.Content>
             </Layout>
         );
