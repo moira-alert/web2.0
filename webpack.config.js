5a7acc537de24f795194e56831b02d6f523b89fb
titusjaka <sescusu@gmail.com>
2018-07-19 18:02:08 +0500

    Change bach webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -130,7 +130,7 @@ const config = argv => {
                       }
                     : {
                           "/api": {
-                              target: "http://localhost:8081",
+                              target: "http://vm-moira-all1:8081",
                               secure: false,
                               changeOrigin: true,
                           },
