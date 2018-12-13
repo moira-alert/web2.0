ec88f36357bb4017ec6133594ddc6b2368935dac
Александр Сергеевич Сушко <sushko@skbkontur.ru>
2018-12-13 12:38:40 +0500

    Rename button

--- src/Components/MetricList/MetricList.js
+++ src/Components/MetricList/MetricList.js
@@ -72,7 +72,7 @@ export default function MetricList(props: Props): React.Node {
                 <div className={cn("controls")}>
                     {onNoDataRemove && (
                         <Button use="link" icon="Trash" onClick={() => onNoDataRemove()}>
-                            Delete NODATA metrics
+                            Delete all NODATA
                         </Button>
                     )}
                 </div>
