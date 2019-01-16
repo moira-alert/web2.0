018621263e440dd68bb7273fced5183d3e50414d
Александр Сергеевич Сушко <sushko@skbkontur.ru>
2019-01-16 11:00:48 +0500

    Replace npm package by css rule for element opacity

--- src/Components/StatusIndicator/StatusIndicator.js
+++ src/Components/StatusIndicator/StatusIndicator.js
@@ -1,40 +1,31 @@
 // @flow
 import * as React from "react";
-import color from "color";
 import type { Status } from "../../Domain/Status";
-import { Statuses, getStatusColor as defaultGetStatusColor } from "../../Domain/Status";
+import { Statuses, getStatusColor } from "../../Domain/Status";
 import cn from "./StatusIndicator.less";
-
-type GetStatusColor = (Status, ?boolean) => string;
 
 type Props = {|
     disabled?: boolean,
     statuses: Array<Status>,
     size?: number,
-    getStatusColor?: GetStatusColor,
 |};
 
-function renderPath(statuses: Array<Status>, disabled: ?boolean, getStatusColorEx: GetStatusColor): React.Element<any> {
+function renderPath(statuses: Array<Status>): React.Element<any> {
     const [status1, status2, status3] = statuses;
 
     switch (statuses.length) {
         case 1:
             if (status1 === Statuses.EXCEPTION) {
-                return (
-                    <circle cx="0" cy="0" r="1" fill={getStatusColorEx(status1, disabled)} className={cn("blink")} />
-                );
+                return <circle cx="0" cy="0" r="1" fill={getStatusColor(status1)} className={cn("blink")} />;
             }
-            return <circle cx="0" cy="0" r="1" fill={getStatusColorEx(status1, disabled)} />;
+            return <circle cx="0" cy="0" r="1" fill={getStatusColor(status1)} />;
         case 2:
             return (
                 <g>
-                    <path
-                        d="M 1 0 A 1 1 0 0 1 -1 1.2246467991473532e-16 L 0 0"
-                        fill={getStatusColorEx(status1, disabled)}
-                    />
+                    <path d="M 1 0 A 1 1 0 0 1 -1 1.2246467991473532e-16 L 0 0" fill={getStatusColor(status1)} />
                     <path
                         d="M -1 1.2246467991473532e-16 A 1 1 0 0 1 1 -2.4492935982947064e-16 L 0 0"
-                        fill={getStatusColorEx(status2, disabled)}
+                        fill={getStatusColor(status2)}
                     />
                 </g>
             );
@@ -43,15 +34,15 @@ function renderPath(statuses: Array<Status>, disabled: ?boolean, getStatusColorE
                 <g>
                     <path
                         d="M 1 0 A 1 1 0 0 1 -0.48175367410171543 0.8763066800438635 L 0 0"
-                        fill={getStatusColorEx(status1, disabled)}
+                        fill={getStatusColor(status1)}
                     />
                     <path
                         d="M -0.48175367410171543 0.8763066800438635 A 1 1 0 0 1 -0.48175367410171527 -0.8763066800438636 L 0 0"
-                        fill={getStatusColorEx(status2, disabled)}
+                        fill={getStatusColor(status2)}
                     />
                     <path
                         d="M -0.48175367410171527 -0.8763066800438636 A 1 1 0 0 1 1 -2.4492935982947064e-16 L 0 0"
-                        fill={getStatusColorEx(status3, disabled)}
+                        fill={getStatusColor(status3)}
                     />
                 </g>
             );
@@ -64,22 +55,14 @@ export default function StatusIndicator(props: Props): React.Node {
     const OPTIONS = {
         size: 20,
     };
-    const { statuses, disabled, size, getStatusColor = defaultGetStatusColor } = props;
-
-    function getStatusColorEx(status: Status, disabled: ?boolean): string {
-        const result = getStatusColor(status);
-        if (disabled) {
-            return color(result)
-                .lighten(0.5)
-                .rgb()
-                .toString();
-        }
-        return result;
-    }
-
+    const { statuses, disabled, size } = props;
     return (
-        <svg viewBox="-1 -1 2 2" width={size || OPTIONS.size} height={size || OPTIONS.size} className={cn("svg")}>
-            {renderPath(statuses, disabled, getStatusColorEx)}
+        <svg
+            viewBox="-1 -1 2 2"
+            width={size || OPTIONS.size}
+            height={size || OPTIONS.size}
+            className={cn("svg", { disabled: disabled })}>
+            {renderPath(statuses)}
         </svg>
     );
 }
