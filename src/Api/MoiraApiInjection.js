ecd272771b717e31bf40be923e91094e0a86b372
Александр Сергеевич Сушко <sushko@skbkontur.ru>
2019-01-17 18:09:18 +0500

    Cleanup in flow config and types

--- src/Api/MoiraApiInjection.js
+++ src/Api/MoiraApiInjection.js
@@ -1,4 +1,4 @@
-// @noflow
+// @flow
 import * as React from "react";
 
 const ApiContext = React.createContext();
--- src/Helpers/PromiseUtils.js
+++ src/Helpers/PromiseUtils.js
@@ -1,4 +1,4 @@
-// @noflow
+// @flow
 export function delay(timeout: number): Promise<() => void> {
     return new Promise(resolve => setTimeout(resolve, timeout));
 }
