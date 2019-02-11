9e1e6e8d8c7509c579837444abce409121321aea
Sasha Sushko <sushko@outlook.com>
2019-02-11 19:38:47 +0500

    Revert "Update flow-types"
    This reverts commit 7892ae666cc3e844e6ed3a111adb1a803a8acac4.

--- flow-typed/npm/color-hash_v1.x.x.js
+++ flow-typed/npm/color-hash_v1.x.x.js
@@ -14,18 +14,11 @@
  */
 
 declare module "color-hash" {
-    declare type options = $Exact<{
-      hash: (str: string) => number,
-      lightness: number | Array<number>,
-      saturation: number | Array<number>
-    }>;
-
     declare class ColorHash {
-      constructor(options: $Shape<options>): void;
-      hsl(str: string): Array<number>;
-      rgb(str: string): Array<number>;
-      hex(str: string): string;
+        constructor(options: { lightness: number | Array<number>, saturation: number | Array<number> }): void;
+        hsl(str: string): Array<number>;
+        rgb(str: string): Array<number>;
+        hex(str: string): string;
     }
-
     declare module.exports: Class<ColorHash>;
-  }
+}
