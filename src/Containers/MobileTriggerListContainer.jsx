5a7536bbbd999580c47fd69e61ba3a392c078607
Sasha Sushko <sushko@outlook.com>
2019-02-18 12:27:01 +0500

    Fix moira-alert/moira/issues/177
    (cherry picked from commit cf237e6e2b027fbd03171aa4f30034afc37b5dfa)

--- src/Containers/MobileTriggerListContainer.jsx
+++ src/Containers/MobileTriggerListContainer.jsx
@@ -44,6 +44,7 @@ class TriggerListContainer extends React.Component<Props, State> {
     };
 
     componentDidMount() {
+        document.title = "Moira - Triggers";
         this.getData(this.props);
     }
 
