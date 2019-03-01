35e6a441fcb94aae8f600d239f15b2d1b8fd6f34
lazycipher <imhsingh16@gmail.com>
2019-03-01 20:34:08 +0530

    user added in NotificationList

--- src/Components/NotificationList/NotificationList.jsx
+++ src/Components/NotificationList/NotificationList.jsx
@@ -14,7 +14,7 @@ import ContactTypeIcon from "../ContactTypeIcon/ContactTypeIcon";
 
 type Props = {|
     items: { [id: string]: Notification },
-    onRemove: (key: string) => void,
+onRemove: (key: string) => void,
 |};
 
 export default function NotificationList(props: Props): React.Element<any> {
@@ -23,46 +23,48 @@ export default function NotificationList(props: Props): React.Element<any> {
     return Object.keys(items).length === 0 ? (
         <div className={cn("no-result")}>Empty :-)</div>
     ) : (
-        <Gapped gap={30} vertical>
-            <div className={cn("row", "header")}>
-                <div className={cn("timestamp")}>Timestamp</div>
-                <div className={cn("trigger")}>Trigger</div>
-                <div className={cn("contact")}>Contact</div>
-                <div className={cn("throttled")}>Throttled</div>
-                <div className={cn("fails")}>Fails</div>
-                <div className={cn("remove")} />
-            </div>
-            {Object.keys(items).map(key => {
-                const { timestamp, trigger, contact, throttled, send_fail: fails } = items[key];
-                const { type, value } = contact;
-                const { id, name } = trigger;
-                return (
-                    <div key={id} className={cn("row")}>
-                        <div className={cn("timestamp")}>
-                            {moment.unix(timestamp).format("MMMM D, HH:mm:ss")}
-                        </div>
-                        <div className={cn("trigger")}>
-                            {id ? (
-                                <RouterLink to={getPageLink("trigger", id)}>{name}</RouterLink>
-                            ) : (
-                                <span>&mdash;</span>
-                            )}
-                        </div>
-                        <div className={cn("contact")}>
-                            <ContactTypeIcon type={type} /> {value}
-                        </div>
-                        <div className={cn("throttled", { true: throttled, false: !throttled })}>
-                            {throttled ? <OkIcon /> : <DeleteIcon />}
-                        </div>
-                        <div className={cn("fails")}>{fails}</div>
-                        <div className={cn("remove")}>
-                            <Button use="link" icon={<TrashIcon />} onClick={() => onRemove(key)}>
-                                Remove
+            <Gapped gap={30} vertical>
+                <div className={cn("row", "header")}>
+                    <div className={cn("timestamp")}>Timestamp</div>
+                    <div className={cn("trigger")}>Trigger</div>
+                    <div className={cn("user")}>User</div>
+                    <div className={cn("contact")}>Contact</div>
+                    <div className={cn("throttled")}>Throttled</div>
+                    <div className={cn("fails")}>Fails</div>
+                    <div className={cn("remove")} />
+                </div>
+                {Object.keys(items).map(key => {
+                    const { timestamp, trigger, contact, throttled, send_fail: fails } = items[key];
+                    const { type, value, user } = contact;
+                    const { id, name } = trigger;
+                    return (
+                        <div key={id} className={cn("row")}>
+                            <div className={cn("timestamp")}>
+                                {moment.unix(timestamp).format("MMMM D, HH:mm:ss")}
+                            </div>
+                            <div className={cn("trigger")}>
+                                {id ? (
+                                    <RouterLink to={getPageLink("trigger", id)}>{name}</RouterLink>
+                                ) : (
+                                        <span>&mdash;</span>
+                                    )}
+                            </div>
+                            <div className={cn("user")}>{user}</div>
+                            <div className={cn("contact")}>
+                                <ContactTypeIcon type={type} /> {value}
+                            </div>
+                            <div className={cn("throttled", { true: throttled, false: !throttled })}>
+                                {throttled ? <OkIcon /> : <DeleteIcon />}
+                            </div>
+                            <div className={cn("fails")}>{fails}</div>
+                            <div className={cn("remove")}>
+                                <Button use="link" icon={<TrashIcon />} onClick={() => onRemove(key)}>
+                                    Remove
                             </Button>
+                            </div>
                         </div>
-                    </div>
-                );
-            })}
-        </Gapped>
-    );
+                    );
+                })}
+            </Gapped>
+        );
 }
