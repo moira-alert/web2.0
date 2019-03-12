5a6e774635aad63148a244b428637c5d9e3f90d1
Sasha Sushko <sushko@outlook.com>
2019-03-12 17:14:16 +0500

    Edit data in stories for new config contact contract

--- src/Stories/ContactEditForm.stories.js
+++ src/Stories/ContactEditForm.stories.js
@@ -5,7 +5,6 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { ValidationContainer } from "react-ui-validations";
 import ContactEditForm from "../Components/ContactEditForm/ContactEditForm";
-import { ContactTypes } from "../Domain/ContactType";
 import contactConfigs from "./Data/ContactConfigs";
 
 const commonProps = {
@@ -15,30 +14,13 @@ const commonProps = {
 
 storiesOf("ContactEditForm", module)
     .addDecorator(story => <ValidationContainer>{story()}</ValidationContainer>)
-    .add("EmptyForm", () => (
+    .add("empty", () => <ContactEditForm {...commonProps} contactInfo={null} />)
+    .add("filled", () => (
         <ContactEditForm
             {...commonProps}
             contactInfo={{
-                type: null,
-                value: "",
-            }}
-        />
-    ))
-    .add("PushoverType", () => (
-        <ContactEditForm
-            {...commonProps}
-            contactInfo={{
-                type: ContactTypes.pushover,
-                value: "",
-            }}
-        />
-    ))
-    .add("TelegramType", () => (
-        <ContactEditForm
-            {...commonProps}
-            contactInfo={{
-                type: ContactTypes.telegram,
-                value: "",
+                type: "email",
+                value: "test@email",
             }}
         />
     ));
