876d61464843a40d20d74b62185af27124862ae6
idoko <radpangolin@gmail.com>
2020-02-20 00:04:17 +0100

    refactor PatternListItem to it's own component

---
+++ src/Components/PatternListItem/PatternListItem.jsx
@@ -0,0 +1,80 @@
+// @flow
+import * as React from "react";
+import TrashIcon from "@skbkontur/react-icons/Trash";
+import { Button } from "@skbkontur/react-ui/components/Button";
+import cn from "../PatternList/PatternList.less";
+import RouterLink from "../RouterLink/RouterLink";
+import { getPageLink } from "../../Domain/Global";
+import type { Pattern } from "../../Domain/Pattern";
+
+type ItemProps = {
+    data: Pattern,
+    onRemove: () => void,
+};
+type ItemState = {
+    showInfo: boolean,
+};
+
+export default class PatternListItem extends React.Component<ItemProps, ItemState> {
+    props: ItemProps;
+
+    state: ItemState = {
+        showInfo: false,
+    };
+
+    render(): React.Node {
+        const { data, onRemove } = this.props;
+        const { pattern, triggers, metrics } = data;
+        const { showInfo } = this.state;
+        const isTriggers = triggers.length !== 0;
+        const isMetrics = metrics.length !== 0;
+        return (
+            <div className={cn("row", { active: showInfo, clicable: isTriggers || isMetrics })}>
+                {isTriggers || isMetrics ? (
+                    <button
+                        type="button"
+                        className={cn("name", "clicked")}
+                        onClick={() => this.setState({ showInfo: !showInfo })}
+                    >
+                        {pattern}
+                    </button>
+                ) : (
+                    <div className={cn("name")}>{pattern}</div>
+                )}
+                <div className={cn("trigger-counter")}>{triggers.length}</div>
+                <div className={cn("metric-counter")}>{metrics.length}</div>
+                <div className={cn("control")}>
+                    <Button use="link" icon={<TrashIcon />} onClick={() => onRemove()}>
+                        Delete
+                    </Button>
+                </div>
+                {showInfo && (
+                    <div className={cn("info")}>
+                        {isTriggers && (
+                            <div className={cn("group")}>
+                                <b>Triggers</b>
+                                {triggers.map(({ id, name }) => (
+                                    <div key={id} className={cn("item")}>
+                                        <RouterLink to={getPageLink("trigger", id)}>
+                                            {name}
+                                        </RouterLink>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                        {isMetrics && (
+                            <div className={cn("group")}>
+                                <b>Metrics</b>
+                                {metrics.map(metric => (
+                                    <div key={metric} className={cn("item")}>
+                                        {metric}
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
