827791553ac12ada913cc5c9b81e4cd3f1b21744
Harshit Verma <hverma1@cs.iitr.ac.in>
2020-03-26 11:04:31 +0530

    feat(EventList, MetricEvents): added event counts column

--- src/Components/EventList/EventList.jsx
+++ src/Components/EventList/EventList.jsx
@@ -6,7 +6,7 @@ import MetricEvents from "../MetricEvents/MetricEvents";
 
 type Props = {|
     items: {
-        [key: string]: Array<Event>,
+        [key: string]: Array<Array<Event>, Number>,
     },
 |};
 
@@ -17,12 +17,13 @@ export default function EventList(props: Props): React.Node {
             <div className={cn("row", "header")}>
                 <div className={cn("name")}>Metric</div>
                 <div className={cn("state-change")}>State change</div>
+                <div className={cn("count")}>Events count</div>
                 <div className={cn("date")}>Event time</div>
                 <div className={cn("dropdown")} />
             </div>
             {Object.keys(items).map(key => (
                 <div key={key} className={cn("group")}>
-                    <MetricEvents metricName={key} events={items[key]} />
+                    <MetricEvents metricName={key} events={items[key][0]} count={items[key][1]} />
                 </div>
             ))}
         </section>
