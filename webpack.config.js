16317f31e06d7a44eba589ee3da8ebbe6b3bb2e4
Petr Gilevich <unvir@yandex.ru>
2020-07-26 23:33:50 +0300

    Updated configs

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,7 @@ module.exports = {
                 exclude: /node_modules/,
                 use: [
                     {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env', '@babel/preset-react']
-                        }
+                        loader: 'babel-loader'
                     }
                 ]
             },
