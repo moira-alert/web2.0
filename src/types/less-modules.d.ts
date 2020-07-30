5d9d76017c330e9f48737342eebe5603843f8fb6
Petr Gilevich <unvir@yandex.ru>
2020-07-30 18:54:56 +0300

    Updated less-module import

--- src/types/less-modules.d.ts
+++ src/types/less-modules.d.ts
@@ -1,4 +1,5 @@
 declare module "*.less" {
+    // eslint-disable-next-line prettier/prettier
     type CssModules = classNames | ((name: string) => string);
     declare const less = CssModules;
     export default less;
