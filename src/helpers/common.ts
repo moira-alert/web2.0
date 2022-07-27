881323ed4dbf351038a725f5fd641f908bbe94a0
solovev.aa <solovev.aa@skbkontur.ru>
2022-07-27 18:02:16 +0700

    extract clearInput to helpers

--- src/helpers/common.ts
+++ src/helpers/common.ts
@@ -1,3 +1,12 @@
 export function notUndefined<T>(x?: T): x is T {
     return x !== undefined;
 }
+
+export const clearInput = (input: string | Array<string>): string => {
+    let cleared = Array.isArray(input) ? input.join(" ") : input;
+
+    cleared = cleared.trim();
+    cleared = cleared.replace(/[\s]+/g, " ");
+
+    return cleared;
+};
