069d64ef6aa24d14bff0d9e9dd0233841c53e027
sol-un <solovev.aa@skbkontur.ru>
2022-09-10 14:19:51 +0300

    finish advanced mode test

--- playwright/triggerCRUD.spec.ts
+++ playwright/triggerCRUD.spec.ts
@@ -29,37 +29,38 @@ test.describe("Trigger CRUD", () => {
 
         await test.step("Try to create trigger from empty form", async () => {
             await createTriggerButton.click();
-            await expect(form.getMessageNear(nameInput, "Can't be empty")).toBeVisible();
+
             await targetInput.hover();
-            await expect(form.getMessageNear(targetInput, "Can't be empty")).toBeVisible();
+            await expect(page.locator("text=Can't be empty")).toHaveCount(2);
+
             await warnRisingValueInput.hover();
             await expect(
-                form.getMessageNear(
-                    warnRisingValueInput,
-                    "At least one of the values must be filled"
-                )
+                page.locator("text=At least one of the values must be filled")
             ).toBeVisible();
+
             await tagsInput.hover();
-            await expect(form.getMessageNear(tagsInput, "Select at least one tag")).toBeVisible();
+            await expect(page.locator("text=Select at least one tag")).toBeVisible();
         });
 
         await test.step("Try to create trigger with wrong Target", async () => {
             await nameInput.fill("test name");
             await descriptionInput.fill("test description");
-            await targetInput.fill("wrong target");
             await warnRisingValueInput.fill("2");
             await tagsInput.fill("test tag");
             await form.getTagBadgeByName("test tag").click();
+
+            await targetInput.fill("wrong target");
             await createTriggerButton.click();
             await expect(
-                form.getMessageNear(
-                    targetInput,
-                    "Function is not supported, if you want to use it, switch to remote"
+                page.locator(
+                    "text=Function is not supported, if you want to use it, switch to remote"
                 )
             ).toBeVisible();
+
             await targetInput.fill("sumSeries(test.target.*");
             await createTriggerButton.click();
-            await expect(form.getMessageNear(targetInput, "Syntax error")).toBeVisible();
+            await expect(page.locator("text=Syntax error")).toBeVisible();
+
             await targetInput.type(")");
         });
 
@@ -70,42 +71,64 @@ test.describe("Trigger CRUD", () => {
                 await createTriggerButton.click();
                 await errorRisingValueInput.hover();
                 await expect(
-                    form.getMessageNear(
-                        errorRisingValueInput,
-                        "Error value must be greater than Warn value"
-                    )
+                    page.locator("text=Error value must be greater than Warn value")
                 ).toBeVisible();
                 await errorRisingValueInput.fill("3");
 
                 await simpleModeFallingRadio.click();
+
                 await warnFallingValueInput.fill("1");
                 await errorFallingValueInput.fill("2");
                 await createTriggerButton.click();
                 await errorFallingValueInput.hover();
                 await expect(
-                    form.getMessageNear(
-                        errorFallingValueInput,
-                        "Error value must be less than Warn value"
-                    )
+                    page.locator("text=Error value must be less than Warn value")
                 ).toBeVisible();
                 await warnFallingValueInput.fill("3");
             }
         );
 
         await test.step(
-            "Try to create trigger in Advanced mode with wrong Target/Expression",
+            "Try to create trigger in Advanced mode with wrong second Target, Expression",
             async () => {
                 await addTargetButton.click();
-                await targetInput.nth(1).fill("sumSeries(test.target.*)");
+
+                await createTriggerButton.click();
+                await expect(page.locator("text=Can't be empty")).toBeVisible();
+                await expressionInput.hover();
+                await expect(page.locator("text=Expression can't be empty")).toBeVisible();
+
+                await expressionInput.fill("wrong expression");
+                await targetInput.nth(1).fill("wrong target");
+                await createTriggerButton.click();
+                await expect(
+                    page.locator(
+                        "text=Function is not supported, if you want to use it, switch to remote"
+                    )
+                ).toBeVisible();
+                await targetInput.nth(1).fill("sumSeries(test.target.*");
+                await createTriggerButton.click();
+                await expect(page.locator("text=Syntax error")).toBeVisible();
+                await targetInput.nth(1).type(")");
+
+                await createTriggerButton.click();
+                await expect(page.locator("text=Cannot transition token types")).toBeVisible();
+                await expressionInput.fill("t1 > t2");
+                await createTriggerButton.click();
+                await expect(
+                    page.locator("text=expression result must be state value")
+                ).toBeVisible();
                 await expressionInput.fill("t1 > t2 ? ERROR : OK");
             }
         );
 
-        await createTriggerButton.click();
-        await expect(page).toHaveURL(/([a-z\d]+-)+/);
+        await test.step("Create trigger & check for success", async () => {
+            await createTriggerButton.click();
+            await expect(page).toHaveURL(/[a-z\d]+-+/);
 
-        await page.waitForTimeout(1000); // TODO: wait for request to finish instead of timeout
-        await mainPage.goto();
-        await expect(page.locator("text=test name")).toBeVisible;
+            await page.waitForTimeout(1000); // TODO: wait for request to finish instead of timeout
+            await mainPage.goto();
+            await expect(page.locator("text=test name")).toBeVisible;
+        });
     });
 });
