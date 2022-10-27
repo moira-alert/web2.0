cfd43deceb27162091d55dc75971996c19e472cc
dimohqa <dimohqa@skbkontur.ru>
2022-10-27 14:58:02 +0700

    add expose ports for image

--- .creevey/github.config.js
+++ .creevey/github.config.js
@@ -4,6 +4,7 @@ module.exports = {
     screenDir: path.join(__dirname, "images"),
     reportDir: path.join(__dirname, "report"),
     maxRetries: 2,
+    useDocker: false,
     browsers: {
         chrome: {
             browserName: "chrome",
