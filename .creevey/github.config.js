fb86a681dab01dd491cb8678999ee73b80eb4555
dimohqa <dimohqa@skbkontur.ru>
2022-10-27 19:28:47 +0700

    add docker field

--- .creevey/github.config.js
+++ .creevey/github.config.js
@@ -3,13 +3,14 @@ const path = require("path");
 module.exports = {
     screenDir: path.join(__dirname, "images"),
     reportDir: path.join(__dirname, "report"),
+    storybookUrl: "http://localhost:9001",
     maxRetries: 2,
+    useDocker: true,
     browsers: {
         chrome: {
             browserName: "chrome",
             platformName: "linux",
             viewport: { width: 1024, height: 720 },
-            gridUrl: "http://selenium__standalone-chrome:4444",
         },
     },
 };
