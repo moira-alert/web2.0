744a9a8fe4c75103722d5ec8687e75a8d3e01f1c
sol-un <solovev.aa@skbkontur.ru>
2022-10-28 12:17:31 +0300

    fix TriggerDeleteModal test
    for further detail, see https://github.com/creevey/creevey/issues/150

--- src/Stories/TriggerDeleteModal.stories.tsx
+++ src/Stories/TriggerDeleteModal.stories.tsx
@@ -3,20 +3,30 @@ import { action } from "@storybook/addon-actions";
 import { storiesOf } from "@storybook/react";
 import { TriggerDeleteModal } from "../Components/TriggerDeleteModal/TriggerDeleteModal";
 
+const params = { creevey: { captureElement: null } };
+
 storiesOf("TriggerDeleteModal", module)
-    .add("Default", () => (
-        <TriggerDeleteModal
-            triggerName={"test trigger"}
-            onClose={action("onClose")}
-            onDelete={() => new Promise(action("onDelete"))}
-        />
-    ))
-    .add("Long trigger name", () => (
-        <TriggerDeleteModal
-            triggerName={
-                "ke.notifications-dev.mail-sender.alive.cloud.noname.*.all.metrics.few.error.one.warning.zero.nodata.min.ok"
-            }
-            onClose={action("onClose")}
-            onDelete={() => new Promise(action("onDelete"))}
-        />
-    ));
+    .add(
+        "Default",
+        () => (
+            <TriggerDeleteModal
+                triggerName={"test trigger"}
+                onClose={action("onClose")}
+                onDelete={() => new Promise(action("onDelete"))}
+            />
+        ),
+        params
+    )
+    .add(
+        "Long trigger name",
+        () => (
+            <TriggerDeleteModal
+                triggerName={
+                    "ke.notifications-dev.mail-sender.alive.cloud.noname.*.all.metrics.few.error.one.warning.zero.nodata.min.ok"
+                }
+                onClose={action("onClose")}
+                onDelete={() => new Promise(action("onDelete"))}
+            />
+        ),
+        params
+    );
