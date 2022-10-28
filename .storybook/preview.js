a3470a0ca4ad79c2553e8a4813ef2391c787f1c4
sol-un <solovev.aa@skbkontur.ru>
2022-10-28 10:43:23 +0300

    fix storybook not compatible w/ compound components
    for further detail, see https://github.com/storybookjs/storybook/issues/5013

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,13 @@
+import "reflect-metadata";
 import "../src/style.less";
 
 export const parameters = {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
     creevey: {
         captureElement: "#root",
