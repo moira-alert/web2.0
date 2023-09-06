5a5c0f519d421362fb5b55ad7552de7f0a1d423d
Eduard Zaidler <zaidler@skbkontur.ru>
2023-09-06 12:13:17 +0700

    confirm modal

--- src/Components/ConfirmDeleteModal/ConfirmDeleteModal.tsx
+++ src/Components/ConfirmDeleteModal/ConfirmDeleteModal.tsx
@@ -2,17 +2,16 @@ import React from "react";
 import { Button, Gapped, Modal } from "@skbkontur/react-ui";
 
 type Props = {
-    triggerName: string;
+    message: string;
+    children?: React.ReactNode;
     onClose: () => void;
     onDelete: () => Promise<void>;
 };
 
-export const TriggerDeleteModal = (props: Props): JSX.Element => (
+export const ConfirmDeleteModal = (props: Props): JSX.Element => (
     <Modal width={600} onClose={props.onClose}>
-        <Modal.Header>Delete Trigger?</Modal.Header>
-        <Modal.Body data-tid="Delete Trigger Modal Body">
-            Trigger <strong>{props.triggerName}</strong> will be deleted.
-        </Modal.Body>
+        <Modal.Header>{props.message}</Modal.Header>
+        <Modal.Body data-tid="Delete Trigger Modal Body">{props.children}</Modal.Body>
         <Modal.Footer>
             <Gapped gap={8}>
                 <Button onClick={props.onClose} use="primary" data-tid="Close Delete Modal">
--- src/Stories/TriggerDeleteModal.stories.tsx
+++ src/Stories/TriggerDeleteModal.stories.tsx
@@ -1,31 +1,37 @@
 import * as React from "react";
 import { action } from "@storybook/addon-actions";
 import { storiesOf } from "@storybook/react";
-import { TriggerDeleteModal } from "../Components/TriggerDeleteModal/TriggerDeleteModal";
+import { ConfirmDeleteModal } from "../Components/ConfirmDeleteModal/ConfirmDeleteModal";
 import { captureElementParams } from "./Data/captureElementParams";
 
 storiesOf("TriggerDeleteModal", module)
     .add(
         "Default",
         () => (
-            <TriggerDeleteModal
-                triggerName={"test trigger"}
+            <ConfirmDeleteModal
+                message={"Delete Trigger?"}
                 onClose={action("onClose")}
                 onDelete={() => new Promise(action("onDelete"))}
-            />
+            >
+                Trigger <strong>test trigger</strong> will be deleted.
+            </ConfirmDeleteModal>
         ),
         captureElementParams
     )
     .add(
         "Long trigger name",
         () => (
-            <TriggerDeleteModal
-                triggerName={
-                    "ke.notifications-dev.mail-sender.alive.cloud.noname.*.all.metrics.few.error.one.warning.zero.nodata.min.ok"
-                }
+            <ConfirmDeleteModal
+                message={"Delete Trigger?"}
                 onClose={action("onClose")}
                 onDelete={() => new Promise(action("onDelete"))}
-            />
+            >
+                Trigger
+                <strong>
+                    ke.notifications-dev.mail-sender.alive.cloud.noname.*.all.metrics.few.error.one.warning.zero.nodata.min.ok
+                </strong>
+                will be deleted.
+            </ConfirmDeleteModal>
         ),
         captureElementParams
     );
