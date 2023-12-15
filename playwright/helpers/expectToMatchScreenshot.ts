5eb5e1833613e861fc2e5249b9c0fbcaef2cfebe
sol-un <solovev.aa@skbkontur.ru>
2023-12-15 11:49:24 +0300

    add snapshot subfolders

--- playwright/helpers/expectToMatchScreenshot.ts
+++ playwright/helpers/expectToMatchScreenshot.ts
@@ -1,12 +1,12 @@
 import { expect, Page } from "@playwright/test";
 
-export async function expectToMatchScreenshot(page: Page, story: string) {
+export async function expectToMatchScreenshot(page: Page, component: string, story: string) {
     if (story.toLocaleLowerCase().includes("modal")) {
         await expect(page.locator('//*[@data-tid="modal-content"]')).toHaveScreenshot(
             `${story}.png`
         );
     } else {
         const element = page.locator("#root");
-        await expect(element).toHaveScreenshot(`${story}.png`);
+        await expect(element).toHaveScreenshot([component, `${story}.png`]);
     }
 }
--- playwright/screenshotTest.spec.ts
+++ playwright/screenshotTest.spec.ts
@@ -1,16 +1,13 @@
 import { test } from "@playwright/test";
 import componentList from "../resources/componentList.json";
 import { expectToMatchScreenshot } from "./helpers/expectToMatchScreenshot";
-import { configureSnapshotPath } from "./helpers/configureSnapshotPath";
 import { getStoryURL } from "./helpers/getStoryURL";
 
-for (const [component, stories] of Object.entries(componentList)) {
-    test.describe(async () => {
-        for (const story of stories) {
-            test(`${story}`, async ({ page }) => {
-                await page.goto(getStoryURL(story));
-                await expectToMatchScreenshot(page, story);
-            });
-        }
+Object.entries(componentList).forEach(([component, stories]) => {
+    stories.forEach((story) => {
+        test(`${component} – ${story}`, async ({ page }) => {
+            await page.goto(getStoryURL(story));
+            await expectToMatchScreenshot(page, component, story);
+        });
     });
-}
+});
