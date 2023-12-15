d173d16714009ed9a12fa23565eaf7be0bc99fb9
sol-un <solovev.aa@skbkontur.ru>
2023-12-15 11:56:09 +0300

    fix modal snapshots

--- playwright/helpers/expectToMatchScreenshot.ts
+++ playwright/helpers/expectToMatchScreenshot.ts
@@ -2,9 +2,10 @@ import { expect, Page } from "@playwright/test";
 
 export async function expectToMatchScreenshot(page: Page, component: string, story: string) {
     if (story.toLocaleLowerCase().includes("modal")) {
-        await expect(page.locator('//*[@data-tid="modal-content"]')).toHaveScreenshot(
-            `${story}.png`
-        );
+        await expect(page.locator('//*[@data-tid="modal-content"]')).toHaveScreenshot([
+            component,
+            `${story}.png`,
+        ]);
     } else {
         const element = page.locator("#root");
         await expect(element).toHaveScreenshot([component, `${story}.png`]);
