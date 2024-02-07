4c6c0514c69eaccbdb3eb3799df06852c82a41de
sol-un <solovev.aa@skbkontur.ru>
2024-02-07 14:55:16 +0300

    refactor teamsOperations to execute serially

--- playwright/e2eTests/teamsOperations.spec.ts
+++ playwright/e2eTests/teamsOperations.spec.ts
@@ -5,61 +5,69 @@ const test = base.extend<{
     teamName: string;
     teamDescription: string;
     userName: string;
+    teamsPage: TeamsPage;
 }>({
     teamName: "test team name",
     teamDescription: "test team description",
     userName: "test user name",
+    teamsPage: ({ page }, use) => {
+        const teamsPage = new TeamsPage(page);
+        use(teamsPage);
+    },
 });
 
-test("Teams operations", async ({ page, teamName, teamDescription, userName }) => {
-    const teamsPage = new TeamsPage(page);
-    await test.step("Add team", async () => {
-        await teamsPage.gotoTeamsPage();
-        await expect(page).toHaveURL("/teams");
-        await teamsPage.addTeamButton.click();
-        await teamsPage.nameInput("Team name").fill(teamName);
-        await teamsPage.teamDescription.fill(teamDescription);
-        await teamsPage.previewButton.click();
-        await expect(teamsPage.nameInput("Team name")).toHaveAttribute("value", teamName);
-        await expect(page.getByText(teamDescription)).toBeVisible();
+test.describe.configure({ mode: "serial" });
 
-        const addTeamPromise = page.waitForResponse(/api\/teams/);
-        await page.locator("[data-tid='Confirm add team']").click();
-        await addTeamPromise;
-        await expect(page.getByText(teamName)).toBeVisible();
-    });
+test.beforeEach(async ({ teamsPage }) => {
+    teamsPage.gotoTeamsPage();
+});
 
-    await test.step("Edit team description", async () => {
-        await teamsPage.editDescriptionButton.click();
-        await expect(teamsPage.nameInput("Team name")).not.toBeVisible();
-        await expect(teamsPage.teamDescription).toContainText(teamDescription);
-        await teamsPage.teamDescription.fill(teamDescription + " changed");
-        await page.getByRole("button", { name: "Save" }).click();
-        await expect(page.getByText(teamDescription + " changed")).toBeVisible();
-    });
+test("Add team", async ({ page, teamName, teamDescription, teamsPage }) => {
+    await expect(page).toHaveURL("/teams");
+    await teamsPage.addTeamButton.click();
+    await teamsPage.nameInput("Team name").fill(teamName);
+    await teamsPage.teamDescription.fill(teamDescription);
+    await teamsPage.previewButton.click();
+    await expect(teamsPage.nameInput("Team name")).toHaveAttribute("value", teamName);
+    await expect(page.getByText(teamDescription)).toBeVisible();
 
-    await test.step("Add user", async () => {
-        await teamsPage.showUsersButton.click();
-        await page.locator("text=Add User").click();
-        await expect(page.getByText(`Add User to ${teamName}`)).toBeVisible();
-        await teamsPage.nameInput("User name").fill(userName);
-        await teamsPage.addUserModalButton.click();
-        await expect(page.getByText(userName)).toBeVisible();
-    });
+    const addTeamPromise = page.waitForResponse(/api\/teams/);
+    await page.locator("[data-tid='Confirm add team']").click();
+    await addTeamPromise;
+    await expect(page.getByText(teamName)).toBeVisible();
+});
 
-    await test.step("Delete user", async () => {
-        await teamsPage.deleteUserButton(userName).click();
-        await page.getByRole("button", { name: "Confirm" }).click();
-        await expect(
-            page.locator(`:text("${userName}"):right-of(span[data-tid='Delete user ${userName}'])`)
-        ).not.toBeVisible();
-    });
+test("Edit team description", async ({ page, teamDescription, teamsPage }) => {
+    await teamsPage.editDescriptionButton.click();
+    await expect(teamsPage.nameInput("Team name")).not.toBeVisible();
+    await expect(teamsPage.teamDescription).toContainText(teamDescription);
+    await teamsPage.teamDescription.fill(teamDescription + " changed");
+    await page.getByRole("button", { name: "Save" }).click();
+    await expect(page.getByText(teamDescription + " changed")).toBeVisible();
+});
 
-    await test.step("Delete team", async () => {
-        await teamsPage.deleteTeamButton(teamName).click();
-        await page.getByRole("button", { name: "Confirm" }).click();
-        await expect(
-            page.locator(`:text("${teamName}"):right-of(span[data-tid='Delete team ${teamName}'])`)
-        ).not.toBeVisible();
-    });
+test("Add user", async ({ page, teamName, userName, teamsPage }) => {
+    await teamsPage.showUsersButton.click();
+    await page.locator("text=Add User").click();
+    await expect(page.getByText(`Add User to ${teamName}`)).toBeVisible();
+    await teamsPage.nameInput("User name").fill(userName);
+    await teamsPage.addUserModalButton.click();
+    await expect(page.getByText(userName)).toBeVisible();
+});
+
+test("Delete user", async ({ page, userName, teamsPage }) => {
+    await teamsPage.showUsersButton.click();
+    await teamsPage.deleteUserButton(userName).click();
+    await page.getByRole("button", { name: "Confirm" }).click();
+    await expect(
+        page.locator(`:text("${userName}"):right-of(span[data-tid='Delete user ${userName}'])`)
+    ).not.toBeVisible();
+});
+
+test("Delete team", async ({ page, teamName, teamsPage }) => {
+    await teamsPage.deleteTeamButton(teamName).click();
+    await page.getByRole("button", { name: "Confirm" }).click();
+    await expect(
+        page.locator(`:text("${teamName}"):right-of(span[data-tid='Delete team ${teamName}'])`)
+    ).not.toBeVisible();
 });
