4619275a3df1607417e85bbb3cc35594afc95dcf
sol-un <solovev.aa@skbkontur.ru>
2024-02-07 15:00:05 +0300

    rewrite notificationsOpeations to execute serially

--- playwright/e2eTests/notificationsOperations.spec.ts
+++ playwright/e2eTests/notificationsOperations.spec.ts
@@ -7,61 +7,69 @@ const test = base.extend<{
     channelAccountName: string;
     channelAccountNameEdited: string;
     tag: string;
+    notificationsPage: NotificationsPage;
 }>({
     channelType: "E-mail",
     channelTypeEdited: "Telegram",
     channelAccountName: "testmail@test.com",
     channelAccountNameEdited: "#testtelegramaccount",
     tag: "testTag",
+    notificationsPage: ({ page }, use) => {
+        const notificationsPage = new NotificationsPage(page);
+        use(notificationsPage);
+    },
 });
 
-test("Notifications operations", async ({
+test.describe.configure({ mode: "serial" });
+
+test.beforeEach(async ({ notificationsPage }) => {
+    await notificationsPage.gotoNotificationsPage();
+});
+
+test("Add delivery channel", async ({
     channelType,
+    channelAccountName,
+    page,
+    notificationsPage,
+}) => {
+    await notificationsPage.addDeliveryChannelButton.click();
+    await notificationsPage.modalSelectChannelTypeButton.click();
+    await page.getByRole("button", { name: `${channelType}` }).click();
+    await page.locator(`input:below(:text('${channelType}'))`).fill(channelAccountName);
+    await notificationsPage.modalActionDeliveryChannelButton("Add").click();
+    await expect(page.getByText(channelAccountName)).toBeVisible();
+});
+
+test("Edit existing delivery channel", async ({
     channelTypeEdited,
     channelAccountName,
     channelAccountNameEdited,
-    tag,
     page,
+    notificationsPage,
 }) => {
-    const notificationsPage = new NotificationsPage(page);
+    notificationsPage.deliveryChannelItem(channelAccountName).click();
+    await notificationsPage.modalSelectChannelTypeButton.click();
+    await page.getByRole("button", { name: `${channelTypeEdited}` }).click();
+    await page.locator(`input:below(:text('${channelTypeEdited}'))`).clear();
+    await page.locator(`input:below(:text('${channelTypeEdited}'))`).fill(channelAccountNameEdited);
+    await notificationsPage.modalActionDeliveryChannelButton("Save").click();
+});
 
-    await test.step("Add delivery channel", async () => {
-        await notificationsPage.gotoNotificationsPage();
-        await notificationsPage.addDeliveryChannelButton.click();
-        await notificationsPage.modalSelectChannelTypeButton.click();
-        await page.getByRole("button", { name: `${channelType}` }).click();
-        await page.locator(`input:below(:text('${channelType}'))`).fill(channelAccountName);
-        await notificationsPage.modalActionDeliveryChannelButton("Add").click();
-        await expect(page.getByText(channelAccountName)).toBeVisible();
-    });
+test("Add subscription", async ({ channelAccountNameEdited, tag, page, notificationsPage }) => {
+    await notificationsPage.addSubscriptionButton.click();
+    await notificationsPage.modalSelectDeliveryChannelButton.click();
+    await page.locator(`button:has-text("${channelAccountNameEdited}")`).click();
+    await page.locator("[data-tid='Tag dropdown select']").click();
+    await page.locator(`[data-tid='Tag ${tag}']`).click();
+    await notificationsPage.modalActionDeliveryChannelButton("Add").click();
+});
 
-    await test.step("Edit existing delivery channel", async () => {
-        notificationsPage.deliveryChannelItem(channelAccountName).click();
-        await notificationsPage.modalSelectChannelTypeButton.click();
-        await page.getByRole("button", { name: `${channelTypeEdited}` }).click();
-        await page.locator(`input:below(:text('${channelTypeEdited}'))`).clear();
-        await page
-            .locator(`input:below(:text('${channelTypeEdited}'))`)
-            .fill(channelAccountNameEdited);
-        await notificationsPage.modalActionDeliveryChannelButton("Save").click();
-    });
+test("Delete subscription", async ({ channelAccountNameEdited, page, notificationsPage }) => {
+    await page.getByText(channelAccountNameEdited).nth(1).click();
+    await notificationsPage.modalActionDeliveryChannelButton("Delete").click();
+});
 
-    await test.step("Add subscription", async () => {
-        await notificationsPage.addSubscriptionButton.click();
-        await notificationsPage.modalSelectDeliveryChannelButton.click();
-        await page.locator(`button:has-text("${channelAccountNameEdited}")`).click();
-        await page.locator("[data-tid='Tag dropdown select']").click();
-        await page.locator(`[data-tid='Tag ${tag}']`).click();
-        await notificationsPage.modalActionDeliveryChannelButton("Add").click();
-    });
-
-    await test.step("Delete subscription", async () => {
-        await page.getByText(channelAccountNameEdited).nth(1).click();
-        await notificationsPage.modalActionDeliveryChannelButton("Delete").click();
-    });
-
-    await test.step("Delete delivery channel", async () => {
-        await page.getByText(channelAccountNameEdited).first().click();
-        await notificationsPage.modalActionDeliveryChannelButton("Delete").click();
-    });
+test("Delete delivery channel", async ({ channelAccountNameEdited, page, notificationsPage }) => {
+    await page.getByText(channelAccountNameEdited).first().click();
+    await notificationsPage.modalActionDeliveryChannelButton("Delete").click();
 });
