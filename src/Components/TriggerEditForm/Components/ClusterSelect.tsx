b6081a9889d71a2288ad2730f52873012135ceb0
EduardZaydler <117674342+EduardZaydler@users.noreply.github.com>
2024-02-15 15:50:20 +0700

    displayname error

--- src/Components/TriggerEditForm/Components/ClusterSelect.tsx
+++ src/Components/TriggerEditForm/Components/ClusterSelect.tsx
@@ -11,26 +11,28 @@ interface IClusterSelectProps {
     onChange: (trigger: Partial<Trigger>, targetIndex?: number) => void;
 }
 
-export const ClusterSelect = React.forwardRef<Select<string | null>, IClusterSelectProps>(function (
-    { clusterID, metricSourceClusters, triggerSource, error, onChange },
-    validationRef
-) {
-    const clusterIDs = metricSourceClusters?.reduce((acc: string[], item) => {
-        if (item.trigger_source === triggerSource) {
-            acc.push(item.cluster_id);
-        }
-        return acc;
-    }, []);
+export const ClusterSelect = React.forwardRef<Select<string | null>, IClusterSelectProps>(
+    function ClusterSelect(
+        { clusterID, metricSourceClusters, triggerSource, error, onChange },
+        validationRef
+    ) {
+        const clusterIDs = metricSourceClusters?.reduce((acc: string[], item) => {
+            if (item.trigger_source === triggerSource) {
+                acc.push(item.cluster_id);
+            }
+            return acc;
+        }, []);
 
-    return (
-        <Select<string | null>
-            className={error ? "validationError" : ""}
-            ref={validationRef}
-            value={clusterID}
-            renderItem={(value) => value}
-            renderValue={(value) => (!clusterID ? null : value)}
-            items={clusterIDs}
-            onValueChange={(value: string | null) => onChange({ cluster_id: value })}
-        />
-    );
-});
+        return (
+            <Select<string | null>
+                className={error ? "validationError" : ""}
+                ref={validationRef}
+                value={clusterID}
+                renderItem={(value) => value}
+                renderValue={(value) => (!clusterID ? null : value)}
+                items={clusterIDs}
+                onValueChange={(value: string | null) => onChange({ cluster_id: value })}
+            />
+        );
+    }
+);
