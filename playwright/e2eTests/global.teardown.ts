0cb2e13c2c2dfb7866c3d5304565e0c916bdc34b
EduardZaydler <117674342+EduardZaydler@users.noreply.github.com>
2024-02-20 18:31:40 +0700

    delete tag request

--- playwright/e2eTests/global.teardown.ts
+++ playwright/e2eTests/global.teardown.ts
@@ -1,11 +1,6 @@
 import { test as cleanup, expect } from "@playwright/test";
 
 cleanup("Deleting tag", async ({ request }) => {
-    const response = await request.delete("/api/tag", {
-        data: {
-            list: ["testTag"],
-        },
-    });
-
+    const response = await request.delete("/api/tag/testTag");
     expect(response.status()).toBe(200);
 });
