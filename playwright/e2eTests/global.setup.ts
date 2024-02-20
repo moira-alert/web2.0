d66e67cdef80d103daa3f899af291cd672f06669
EduardZaydler <117674342+EduardZaydler@users.noreply.github.com>
2024-02-20 15:51:55 +0700

    skipping setup for screenshots

--- playwright/e2eTests/global.setup.ts
+++ playwright/e2eTests/global.setup.ts
@@ -1,13 +1,15 @@
 import { test as setup, expect } from "@playwright/test";
 
 setup("Adding tag", async ({ request, context }) => {
-    const response = await request.post("/api/tag", {
-        data: {
-            list: ["testTag"],
-        },
-    });
+    if (!process.argv.includes("--skip-setup")) {
+        const response = await request.post("/api/tag", {
+            data: {
+                list: ["testTag"],
+            },
+        });
 
-    expect(response.status()).toBe(200);
+        expect(response.status()).toBe(200);
 
-    await context.storageState({ path: "storageState.json" });
+        await context.storageState({ path: "storageState.json" });
+    }
 });
--- playwright/e2eTests/global.teardown.ts
+++ playwright/e2eTests/global.teardown.ts
@@ -2,5 +2,7 @@ import { clearDatabase } from "../../src/tests/core/utils";
 import { test as cleanup } from "@playwright/test";
 
 cleanup("Adding tag", async () => {
-    await clearDatabase();
+    if (!process.argv.includes("--skip-setup")) {
+        await clearDatabase();
+    }
 });
