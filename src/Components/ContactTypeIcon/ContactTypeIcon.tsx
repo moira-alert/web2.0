b8ff01677cb9d7abe8cb635b2aa225c9f0f61a4e
EduardZaydler <117674342+EduardZaydler@users.noreply.github.com>
2024-04-01 20:19:49 +0700

    svg icons, dynamic imports, fallback typeToIcon

--- src/Components/ContactTypeIcon/ContactTypeIcon.tsx
+++ src/Components/ContactTypeIcon/ContactTypeIcon.tsx
@@ -1,51 +1,40 @@
-import * as React from "react";
-import TelegramIcon from "@skbkontur/react-icons/Telegram2";
-import MailIcon from "@skbkontur/react-icons/Mail2";
-import FacebookIcon from "@skbkontur/react-icons/Facebook2";
-import TwitterIcon from "@skbkontur/react-icons/Twitter2";
-import PhoneIcon from "@skbkontur/react-icons/Phone2";
-import WhatsappIcon from "@skbkontur/react-icons/WhatsApp2";
-import ViberIcon from "@skbkontur/react-icons/Viber2";
+import React, { useState, useEffect } from "react";
 import SvgIcon from "../SvgIcon/SvgIcon";
-import PushoverLogo from "./pushover-logo.svg";
-import SlackLogo from "./slack-logo.svg";
-import MsTeamsLogo from "./msteams-logo.svg";
-import TwilioLogo from "./twilio-logo.svg";
-import WebhookLogo from "./webhook-logo.svg";
-import DiscordLogo from "./discord-logo.svg";
-import PagerdutyLogo from "./pagerduty-logo.svg";
-import VictoropsLogo from "./victorops-logo.svg";
-import OpsgenieLogo from "./opsgenie-logo.svg";
-import MattermostLogo from "./mattermost-logo.svg";
+import MailLogo from "./mail-logo.svg";
+import { useAppSelector } from "../../store/hooks";
+import { ConfigState } from "../../store/selectors";
+import { FallbackTypeToIcon } from "./FallbackTypeToIcon";
 
 type Props = {
     type: string;
 };
 
-const TypeToIcon: { [key: string]: React.ReactElement } = {
-    slack: <SvgIcon path={SlackLogo} size={15} offsetTop={2} />,
-    msteams: <SvgIcon path={MsTeamsLogo} size={15} offsetTop={2} />,
-    telegram: <TelegramIcon />,
-    facebook: <FacebookIcon />,
-    viber: <ViberIcon />,
-    whatsapp: <WhatsappIcon />,
-    twitter: <TwitterIcon />,
-    mail: <MailIcon />,
-    email: <MailIcon />,
-    pushover: <SvgIcon path={PushoverLogo} size={14} offsetTop={2} />,
-    twilio: <SvgIcon path={TwilioLogo} size={14} offsetTop={2} />,
-    webhook: <SvgIcon path={WebhookLogo} size={14} offsetTop={2} />,
-    discord: <SvgIcon path={DiscordLogo} size={14} offsetTop={2} />,
-    pagerduty: <SvgIcon path={PagerdutyLogo} size={14} offsetTop={2} />,
-    victorops: <SvgIcon path={VictoropsLogo} size={14} offsetTop={2} />,
-    opsgenie: <SvgIcon path={OpsgenieLogo} size={14} offsetTop={2} />,
-    mattermost: <SvgIcon path={MattermostLogo} size={14} offsetTop={2} />,
-    phone: <PhoneIcon />,
-    tel: <PhoneIcon />,
-    sms: <PhoneIcon />,
-};
-
 export default function ContactTypeIcon({ type }: Props): React.ReactElement {
-    const iconKey = Object.keys(TypeToIcon).find((key) => type.includes(key));
-    return iconKey ? TypeToIcon[iconKey] : <MailIcon />;
+    const { config } = useAppSelector(ConfigState);
+
+    const contact = config?.contacts.find((contact) => type.includes(contact.type));
+
+    const [svgPath, setSvgPath] = useState<string | null>(null);
+
+    useEffect(() => {
+        import(`./${contact?.logo_uri}`)
+            .then((module) => {
+                setSvgPath(module.default);
+            })
+            .catch((error) => {
+                console.error(`Failed to load SVG: ${error.message}`);
+                setSvgPath(null);
+            });
+    }, [contact?.logo_uri]);
+
+    const fallBack = () => {
+        const iconKey = Object.keys(FallbackTypeToIcon).find((key) => type.includes(key));
+        return iconKey ? (
+            FallbackTypeToIcon[iconKey]
+        ) : (
+            <SvgIcon path={MailLogo} size={16} offsetTop={3} />
+        );
+    };
+
+    return svgPath ? <SvgIcon path={svgPath} size={16} offsetTop={3} /> : fallBack();
 }
