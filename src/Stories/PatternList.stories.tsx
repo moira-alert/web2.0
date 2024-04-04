87cac43045aa37c9b7fb66daa82fe055d0026778
EduardZaydler <117674342+EduardZaydler@users.noreply.github.com>
2024-04-04 14:41:25 +0700

    fix patternlist stories

--- src/Stories/PatternList.stories.tsx
+++ src/Stories/PatternList.stories.tsx
@@ -113,6 +113,6 @@ export const Default = () => (
     <PatternList
         items={items}
         onRemove={action("onRemove")}
-        sortConfig={{ direction: "desc", sortingColumn: "triggers" }}
+        sortConfig={{ direction: "desc", sortingColumn: "metrics" }}
     />
 );
