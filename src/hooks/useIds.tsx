347ec8348cf07d26bd85780b00ddaa24f692582a
EduardZaydler <117674342+EduardZaydler@users.noreply.github.com>
2024-04-11 15:23:45 +0700

    replaced crypto.randomUUID with uuidv4 (#504)

--- src/hooks/useIds.tsx
+++ src/hooks/useIds.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
+// Do not using crypto Api cause some opensource users may use Moira in non-secure browser context
 export const useIds = (initialLength: number) => {
-    const [ids, setIds] = useState<string[]>(
-        Array.from({ length: initialLength }, () => crypto.randomUUID())
-    );
+    const [ids, setIds] = useState<string[]>(Array.from({ length: initialLength }, () => uuidv4()));
 
     return [ids, setIds] as const;
 };
