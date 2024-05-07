2d9b94ff45b08d2af7e8162f81f68a69f8662b3c
EduardZaydler <117674342+EduardZaydler@users.noreply.github.com>
2024-05-07 18:17:49 +0700

    polling sw for update

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -5,6 +5,10 @@ export function register(onUpdate: () => void) {
                 .register("/service-worker.js")
                 .catch(console.error)
                 .then((registration) => {
+                    setInterval(() => {
+                        (registration as ServiceWorkerRegistration).update();
+                        console.log("Checked for update...");
+                    }, 1000 * 10 * 1);
                     (registration as ServiceWorkerRegistration).onupdatefound = () => {
                         const installingWorker = (registration as ServiceWorkerRegistration)
                             .installing;
