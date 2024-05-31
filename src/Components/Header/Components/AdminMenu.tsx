fb0e701db89cf6bb22eb40a087bedba5bb033064
EduardZaydler <117674342+EduardZaydler@users.noreply.github.com>
2024-05-31 14:07:03 +0700

    added admin menu loader

--- src/Components/Header/Components/AdminMenu.tsx
+++ src/Components/Header/Components/AdminMenu.tsx
@@ -7,28 +7,35 @@ import { useGetUserQuery } from "../../../services/UserApi";
 import classNames from "classnames/bind";
 
 import styles from "./AdminMenu.less";
+import { Spinner } from "@skbkontur/react-ui/components/Spinner";
 
 const cn = classNames.bind(styles);
 
 export const AdminMenu: FC = () => {
-    const { data: user } = useGetUserQuery();
+    const { data: user, isLoading: isUserLoading } = useGetUserQuery();
+
+    const isAdminMenuEnabled = user?.auth_enabled && user.role === "admin";
 
     return (
         <>
-            {user?.auth_enabled && user.role === "admin" && (
-                <DropdownMenu
-                    caption={
-                        <>
-                            <Crown />
-                            &nbsp;
-                            <button className={cn("admin-button")}>Admin</button>
-                        </>
-                    }
-                >
-                    <LinkMenuItem link={getPageLink("patterns")}>Patterns</LinkMenuItem>
-                    <LinkMenuItem link={getPageLink("notifications")}>Notifier</LinkMenuItem>
-                    <LinkMenuItem link={getPageLink("tags")}>Tags</LinkMenuItem>
-                </DropdownMenu>
+            {isUserLoading ? (
+                <Spinner type={"mini"} dimmed caption="" />
+            ) : (
+                isAdminMenuEnabled && (
+                    <DropdownMenu
+                        caption={
+                            <>
+                                <Crown />
+                                &nbsp;
+                                <button className={cn("admin-button")}>Admin</button>
+                            </>
+                        }
+                    >
+                        <LinkMenuItem link={getPageLink("patterns")}>Patterns</LinkMenuItem>
+                        <LinkMenuItem link={getPageLink("notifications")}>Notifier</LinkMenuItem>
+                        <LinkMenuItem link={getPageLink("tags")}>Tags</LinkMenuItem>
+                    </DropdownMenu>
+                )
             )}
         </>
     );
