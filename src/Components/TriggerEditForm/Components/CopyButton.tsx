deb0fbd8219dc6e48eb5c2f52261d3b7bf97471a
EduardZaydler <117674342+EduardZaydler@users.noreply.github.com>
2024-09-18 08:54:05 +0500

    fix/added execComand fallback (#537)

--- src/Components/TriggerEditForm/Components/CopyButton.tsx
+++ src/Components/TriggerEditForm/Components/CopyButton.tsx
@@ -7,21 +7,39 @@ interface IProps {
     className?: string;
 }
 
+const pushSuccessfulMessage = () => Toast.push("Text copied to clipboard");
+
 const copy = async (text: string) => {
-    if (!navigator?.clipboard) {
-        return;
-    }
-    try {
-        await navigator.clipboard.writeText(text);
-    } catch (error) {
-        console.log(error);
+    if (navigator?.clipboard) {
+        try {
+            await navigator.clipboard.writeText(text);
+            pushSuccessfulMessage();
+        } catch (error) {
+            console.error(error);
+            Toast.push("Failed to copy text");
+        }
+    } else {
+        // Fallback on execCommand
+        const textarea = Object.assign(document.createElement("textarea"), {
+            value: text,
+            style: { position: "absolute", left: "-9999px" },
+        });
+        document.body.appendChild(textarea);
+        textarea.select();
+
+        const isSuccessful = document.execCommand("copy");
+
+        isSuccessful
+            ? pushSuccessfulMessage()
+            : Toast.push("Failed to copy text, try to launch the app under https");
+
+        document.body.removeChild(textarea);
     }
 };
 
 export const CopyButton: React.FC<IProps> = ({ value, className }) => {
-    const handleCopy = () => {
-        Toast.push("Target value copied");
-        copy(value.trim());
+    const handleCopy = async () => {
+        await copy(value.trim());
     };
 
     return <Button className={className} use="link" icon={<CopyIcon />} onClick={handleCopy} />;
