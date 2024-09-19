a06aecb25fe9493714464468dd9ee78c50123845
EduardZaydler <117674342+EduardZaydler@users.noreply.github.com>
2024-09-19 13:13:59 +0700

    added default shed filling

--- src/Components/DateAndTimeMenu/DateAndTimeMenu.tsx
+++ src/Components/DateAndTimeMenu/DateAndTimeMenu.tsx
@@ -54,18 +54,20 @@ export const DateAndTimeMenu: FC<IDateAndTimeMenuProps> = ({
     return (
         <RenderLayer onClickOutside={closeMenu} onFocusOutside={closeMenu} active={isMenuOpen}>
             <div ref={containerEl}>
-                <ValidationWrapperV1
-                    validationInfo={validateDateAndTime && validateDateAndTime(inputValue)}
-                >
-                    <Input
-                        width={160}
-                        mask={"9999/99/99 99:99:99"}
-                        onFocus={closeMenu}
-                        value={inputValue}
-                        onValueChange={handleValueChange}
-                    />
-                </ValidationWrapperV1>
-                <CalendarIcon className={cn("calendar-icon")} onClick={openMenu} />
+                <div className={cn("input-container")}>
+                    <ValidationWrapperV1
+                        validationInfo={validateDateAndTime && validateDateAndTime(inputValue)}
+                    >
+                        <Input
+                            width={160}
+                            mask={"9999/99/99 99:99:99"}
+                            onFocus={closeMenu}
+                            value={inputValue}
+                            onValueChange={handleValueChange}
+                        />
+                    </ValidationWrapperV1>
+                    <CalendarIcon className={cn("calendar-icon")} onClick={openMenu} />
+                </div>
                 {isMenuOpen && (
                     <DropdownContainer
                         offsetY={5}
