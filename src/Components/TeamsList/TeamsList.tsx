6295cbe98e1e5cf19aa3712236977648861022b6
EduardZaydler <117674342+EduardZaydler@users.noreply.github.com>
2024-12-10 15:33:34 +0700

    review

--- src/Components/TeamsList/TeamsList.tsx
+++ src/Components/TeamsList/TeamsList.tsx
@@ -6,24 +6,18 @@ import { Team } from "../../Domain/Team";
 
 export const TeamsList: FC<{ teams?: Team[] }> = ({ teams }) => {
     const [deletingTeam, setDeletingTeam] = useState<Team | null>(null);
-
+    if (!teams?.length) return <EmptyListText text={"There are no teams"} />;
     return (
-        <>
-            {teams?.length ? (
-                <Grid gap="16px" columns="repeat(auto-fit, minmax(300px, 1fr))">
-                    {teams?.map((team) => (
-                        <TeamCard
-                            key={team.id}
-                            team={team}
-                            isDeleting={deletingTeam?.id === team.id}
-                            onOpenDelete={() => setDeletingTeam(team)}
-                            onCloseDelete={() => setDeletingTeam(null)}
-                        />
-                    ))}
-                </Grid>
-            ) : (
-                <EmptyListText text={"There are no teams"} />
-            )}
-        </>
+        <Grid gap="16px" columns="repeat(auto-fit, minmax(300px, 1fr))">
+            {teams?.map((team) => (
+                <TeamCard
+                    key={team.id}
+                    team={team}
+                    isDeleting={deletingTeam?.id === team.id}
+                    onOpenDelete={() => setDeletingTeam(team)}
+                    onCloseDelete={() => setDeletingTeam(null)}
+                />
+            ))}
+        </Grid>
     );
 };
