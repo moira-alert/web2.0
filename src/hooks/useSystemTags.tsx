52caf25ec77b78927c4f6f1489d0317a48b18a21
EduardZaydler <117674342+EduardZaydler@users.noreply.github.com>
2025-04-04 18:55:00 +0700

    screenshots

--- src/hooks/useSystemTags.tsx
+++ src/hooks/useSystemTags.tsx
@@ -4,7 +4,7 @@ import { useGetSystemTagsQuery } from "../services/TagsApi";
 export const useHasSystemTags = (
     tags: string[]
 ): { hasSystemTags: boolean; systemTags: string[] } => {
-    const { data: systemTags = [] } = useGetSystemTagsQuery();
+    const { data: systemTags = [] } = useGetSystemTagsQuery({ handleLoadingLocally: true });
 
     const hasSystemTags = useMemo(() => {
         return tags.some((tag) => systemTags.includes(tag));
