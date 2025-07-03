0bb39d25ff497e300510841dda9c818b020c0c6f
EduardZaydler <117674342+EduardZaydler@users.noreply.github.com>
2025-07-03 19:05:31 +0700

    Chore/sentry update (#601)

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -2,21 +2,6 @@ export function register(onUpdate: () => void) {
     if ("serviceWorker" in navigator) {
         window.addEventListener("load", async () => {
             try {
-                // удалить позже
-                const registrations = await navigator.serviceWorker.getRegistrations();
-                for (const reg of registrations) {
-                    if (reg.active && reg.active.scriptURL.includes("service-worker.js")) {
-                        await reg.unregister();
-                    }
-                }
-
-                const cacheNames = await caches.keys();
-                for (const cacheName of cacheNames) {
-                    if (cacheName === "html-pages") {
-                        await caches.delete(cacheName);
-                    }
-                }
-                //
                 const registration = await navigator.serviceWorker.register("/sw.js");
 
                 registration.onupdatefound = () => {
