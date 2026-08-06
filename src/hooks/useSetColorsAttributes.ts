67d7d2eb25361f2643a692fa20ef8c08ef087276
EduardZaydler <117674342+EduardZaydler@users.noreply.github.com>
2026-08-06 13:55:26 +0700

    fix mobile css

--- src/hooks/useSetColorsAttributes.ts
+++ src/hooks/useSetColorsAttributes.ts
@@ -5,7 +5,7 @@ import { MOIRA_BRAND_COLOR } from "../Constants/moiraBrandColor";
 export const useSetColorsAttributes = (theme: string) => {
     useEffect(() => {
         const root = document.body;
-
+        root.setAttribute("data-theme", theme);
         root.setAttribute("data-k-brand", MOIRA_BRAND_COLOR);
         root.setAttribute("data-k-accent", "gray");
         root.setAttribute("data-k-theme", theme === EThemesNames.Dark ? "dark" : "light");
